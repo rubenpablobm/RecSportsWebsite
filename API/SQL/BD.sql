4868c0800d1bb1535665b7bc917864ead7fa06a9
Jesús Sebastián Jaime Oviedo <a01412442@tec.mx>
2023-04-17 12:47:28 -0600

    17 abr

--- API/SQL/BD.sql
+++ API/SQL/BD.sql
@@ -1,5 +1,77 @@
 /*CREACION DE LA BASE DE DATOS*/
 /*https://chartio.com/resources/tutorials/how-to-define-an-auto-increment-primary-key-in-sql-server/ */
+-- TSQL
+
+CREATE TABLE Edificio
+(
+  IdEdificio INT IDENTITY(1,1) NOT NULL PRIMARY KEY,
+  Nombre NVARCHAR(255) NOT NULL,
+  Foto NVARCHAR(255) NOT NULL,
+  LinkMaps NVARCHAR(255) NOT NULL
+);
+
+CREATE TABLE Admin
+(
+  IdAdmin INT IDENTITY(1,1) NOT NULL PRIMARY KEY,
+  Correo NVARCHAR(255) NOT NULL,
+  Contraseña NVARCHAR(255) NOT NULL
+);
+
+CREATE TABLE Alumno
+(
+  Matricula CHAR(9) NOT NULL,
+  IdAlumno INT IDENTITY(1,1) NOT NULL PRIMARY KEY
+);
+
+CREATE TABLE Aforo
+(
+  Aforo INT NOT NULL,
+  Capacidad INT NOT NULL,
+  IdArea INT NOT NULL UNIQUE,
+  PRIMARY KEY (IdArea),
+  FOREIGN KEY (IdArea) REFERENCES Area(IdArea)
+);
+
+CREATE TABLE Area
+(
+  IdArea INT IDENTITY(1,1) NOT NULL PRIMARY KEY,
+  Nombre NVARCHAR(255) NOT NULL,
+  Foto NVARCHAR(255) NOT NULL,
+  Croquis NVARCHAR(255) NOT NULL,
+  Tipo NVARCHAR(255) NOT NULL,
+  LinkCalendar NVARCHAR(255),
+  Descripcion NVARCHAR(255) NOT NULL,
+  Horarios NVARCHAR(255) NOT NULL,
+  Avisos NVARCHAR(255),
+  IdEdificio INT NOT NULL,
+  idAforo INT,
+  FOREIGN KEY (IdEdificio) REFERENCES Edificio(IdEdificio),
+  FOREIGN KEY (idAforo) REFERENCES Aforo(idAforo),
+  UNIQUE (idAforo)
+);
+
+CREATE TABLE Hora
+(
+  IdHora INT IDENTITY(1,1) NOT NULL PRIMARY KEY,
+  Timestamp INT NOT NULL,
+  Aforo INT NOT NULL,
+  Capacidad INT NOT NULL,
+  IdArea INT NOT NULL,
+  FOREIGN KEY (IdArea) REFERENCES Area(IdArea)
+);
+
+CREATE TABLE Dia
+(
+  IdDia INT IDENTITY(1,1) NOT NULL PRIMARY KEY,
+  Dia INT NOT NULL,
+  TotalPersonas INT NOT NULL,
+  IdArea INT NOT NULL,
+  FOREIGN KEY (IdArea) REFERENCES Area(IdArea)
+);
+
+
+--MYSQL
+
 USE tc3005b;
 
 CREATE TABLE Edificio
@@ -28,10 +100,11 @@ CREATE TABLE Alumno
 
 CREATE TABLE Aforo
 (
-  idAforo INT NOT NULL AUTO_INCREMENT,
   Aforo INT NOT NULL,
   Capacidad INT NOT NULL,
-  PRIMARY KEY (idAforo)
+  IdArea INT NOT NULL,
+  PRIMARY KEY (IdArea),
+  FOREIGN KEY (IdArea) REFERENCES Area(IdArea)
 );
 
 CREATE TABLE Area
