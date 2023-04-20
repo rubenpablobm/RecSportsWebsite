d1fc37e4dca48ea85057b07e394f70dc216d15c2
rubenpablobm <rubenp.barrazam@gmail.com>
2023-04-19 21:01:41 -0600

    Genera modelo Area y listaAreas en componente

--- RecSports-Libre/src/app/area-card/area-card.component.ts
+++ RecSports-Libre/src/app/area-card/area-card.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Area } from '../models/area';
 
 @Component({
   selector: 'app-area-card',
@@ -6,5 +7,28 @@ import { Component } from '@angular/core';
   styleUrls: ['./area-card.component.css']
 })
 export class AreaCardComponent {
-
+  listaAreas : Area[] = [
+    {
+      nombre:'Tennis',
+      foto:'../assets/images/tenis.jpeg',
+      croquis:'',
+      tipo:'disponibilidad',
+      linkCalendar:'',
+      descripcion:'descripción...',
+      horarios:'8:00am-8:00pm',
+      aviso:'Cerrada el 10 de junio por mantenimiento', 
+      idEdificio:1
+    },
+    {
+      nombre:'Basketball',
+      foto:'../assets/images/tenis.jpeg',
+      croquis:'',
+      tipo:'disponibilidad',
+      linkCalendar:'',
+      descripcion:'descripción...',
+      horarios:'8:00am-8:00pm',
+      aviso:'', 
+      idEdificio:1
+    },
+  ]
 }
