fdfd15251f7522baf9a74476dac615c922288a13
rubenpablobm <rubenp.barrazam@gmail.com>
2023-04-21 07:35:51 -0600

    Agrega el componente area-info al routing module El rotuing module nos va a permitir cambiar la interfaz como si fueran páginas distintas

--- RecSports-Libre/src/app/app-routing.module.ts
+++ RecSports-Libre/src/app/app-routing.module.ts
@@ -1,7 +1,18 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AppComponent } from './app.component';
+import { AreaInfoComponent } from './area-info/area-info.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',  // home
+    component: AppComponent
+  },
+  {
+    path: 'area-info',
+    component: AreaInfoComponent,
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
