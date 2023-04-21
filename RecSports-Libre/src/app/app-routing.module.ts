213979199ddbe96c629a093ab528e43ec257f69a
hectorinl <A01411730@itesm.mx>
2023-04-21 10:35:55 -0600

    Admin
    Hice el header y lo hice responsivo

--- RecSports-Libre/src/app/app-routing.module.ts
+++ RecSports-Libre/src/app/app-routing.module.ts
@@ -1,10 +1,22 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AdminComponent } from './admin/admin.component';
+import { BrowserModule } from '@angular/platform-browser';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: 'admin',
+  component: AdminComponent,pathMatch: "full" },
+];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  imports: [
+    BrowserModule,
+    RouterModule.forRoot([
+      {path: 'admin', component: AdminComponent},
+      {path: '', redirectTo: '/admin', pathMatch: 'full'},
+    ]),
+  ],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
+
--- RecSports-Libre/src/app/app.module.ts
+++ RecSports-Libre/src/app/app.module.ts
@@ -1,6 +1,5 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppRoutingModule } from './app-routing.module';
 import {MatToolbarModule} from '@angular/material/toolbar';
 import {MatButtonModule} from '@angular/material/button';
@@ -9,12 +8,14 @@ import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { NavbarComponent } from './navbar/navbar.component';
 import { HeaderComponent } from './header/header.component';
+import { AdminComponent } from './admin/admin.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     NavbarComponent,
-    HeaderComponent
+    HeaderComponent,
+    AdminComponent
   ],
   imports: [
     BrowserModule,
