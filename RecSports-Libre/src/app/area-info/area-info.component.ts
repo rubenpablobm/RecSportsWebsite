6ff355f63f0c8f884609916aa41d16ef5b5e0009
rubenpablobm <rubenp.barrazam@gmail.com>
2023-04-21 09:57:58 -0600

    Agrega clase no-style-link La clase elimina los estilos de las ligas y colorea el texto en blanco

--- RecSports-Libre/src/app/area-info/area-info.component.ts
+++ RecSports-Libre/src/app/area-info/area-info.component.ts
@@ -3,7 +3,7 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-area-info',
   templateUrl: './area-info.component.html',
-  styleUrls: ['./area-info.component.css']
+  styleUrls: ['./area-info.component.css', '../app.component.css']
 })
 export class AreaInfoComponent {
 
