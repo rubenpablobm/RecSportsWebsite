5e9cf8a81d4180730e6e01fc4aced77360111791
rubenpablobm <rubenp.barrazam@gmail.com>
2023-04-21 07:56:32 -0600

    Mueve area-card a componente home Mueve area-card de app-component a componente home y agrega home a routing module.

--- RecSports-Libre/src/app/app-routing.module.ts
+++ RecSports-Libre/src/app/app-routing.module.ts
@@ -1,12 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { AppComponent } from './app.component';
 import { AreaInfoComponent } from './area-info/area-info.component';
+import { HomeComponent } from './home/home.component';
 
 const routes: Routes = [
   {
     path: '',  // home
-    component: AppComponent
+    component: HomeComponent
   },
   {
     path: 'area-info',
