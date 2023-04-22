eaea63724de38766eabc5049f98f9527b1baca95
rubenpablobm <rubenp.barrazam@gmail.com>
2023-04-22 07:41:36 -0600

    Agrega más tarjetas dummy

--- RecSports-Libre/src/app/area-card/area-card.component.ts
+++ RecSports-Libre/src/app/area-card/area-card.component.ts
@@ -30,5 +30,38 @@ export class AreaCardComponent {
       aviso:'', 
       idEdificio:1
     },
+    {
+      nombre:'Yoga',
+      foto:'https://www.semana.com/resizer/XpO1ppJ0tOduMIim3UjatC94Y2c=/arc-anglerfish-arc2-prod-semana/public/ZP7JALBIIVAC3INWFQRITO6JMQ.jpg',
+      croquis:'',
+      tipo:'instructiva',
+      linkCalendar:'',
+      descripcion:'descripción...',
+      horarios:'L-V 8:00am-8:00pm',
+      aviso:'', 
+      idEdificio:1
+    },
+    {
+      nombre:'Paddel',
+      foto:'https://assets.tennismajors.com/app/uploads/2022/07/14134236/PADEL_SWEDISH_CHAMPIONSHIP-1296x675.jpg',
+      croquis:'',
+      tipo:'disponibilidad',
+      linkCalendar:'',
+      descripcion:'descripción...',
+      horarios:'8:00am-8:00pm',
+      aviso:'', 
+      idEdificio:1
+    },
+    {
+      nombre:'Alberca Borregos',
+      foto:'https://img.gruporeforma.com/imagenes/960x640/6/52/5051851.jpg',
+      croquis:'',
+      tipo:'disponibilidad',
+      linkCalendar:'',
+      descripcion:'descripción...',
+      horarios:'L-V - 8:00am-8:00pm \nS - 9:00am - 5:00pm \nCerrado los domingos por limpieza.',
+      aviso:'Cerrado miércoles 19 de 18:00-20:00 por competencia.', 
+      idEdificio:1
+    },
   ]
 }
