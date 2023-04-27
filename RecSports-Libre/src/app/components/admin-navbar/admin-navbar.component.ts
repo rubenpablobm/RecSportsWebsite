3a68963ba1b3b624f891e297e7545c39cc24d290
Jesús Sebastián Jaime Oviedo <a01412442@tec.mx>
2023-04-27 10:22:37 -0600

    Avances de ServiceCRUD

--- RecSports-Libre/src/app/components/admin-navbar/admin-navbar.component.ts
+++ RecSports-Libre/src/app/components/admin-navbar/admin-navbar.component.ts
@@ -1,10 +1,14 @@
 import { Component } from '@angular/core';
 
+import { CrudService } from 'src/app/service/crud.service';
+
 @Component({
   selector: 'app-admin-navbar',
   templateUrl: './admin-navbar.component.html',
   styleUrls: ['./admin-navbar.component.css']
 })
 export class AdminNavbarComponent {
-
+  getArea(){
+    this.
+  }
 }
