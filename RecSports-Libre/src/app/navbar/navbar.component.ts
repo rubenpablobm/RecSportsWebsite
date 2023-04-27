eebb81a63dda725c42ce507d7905f24e3a725f7f
Jesús Sebastián Jaime Oviedo <a01412442@tec.mx>
2023-04-27 11:54:25 -0600

    Avance de consumir API EdificioGet

--- RecSports-Libre/src/app/navbar/navbar.component.ts
+++ RecSports-Libre/src/app/navbar/navbar.component.ts
@@ -1,4 +1,6 @@
 import { Component, Input, HostListener } from '@angular/core';
+import { Edificio } from '../models/edificio';
+import { CrudService } from '../service/crud.service';
 
 @Component({
   selector: 'app-navbar',
@@ -6,4 +8,21 @@ import { Component, Input, HostListener } from '@angular/core';
   styleUrls: ['./navbar.component.css', '../app.component.css']
 })
 export class NavbarComponent {
+
+  ngOnInit() {
+    this.getEdificios();
+  }
+  
+  listaEdificios : any = [];
+  
+  constructor(public crudService:CrudService){ }
+  
+  getEdificios() {
+    console.log('Llamando edificios...')
+    return this.crudService.EdificioGet().subscribe((data:{}) => {
+      console.log(data);
+      this.listaEdificios = data;
+    })
+  }
+
 }
