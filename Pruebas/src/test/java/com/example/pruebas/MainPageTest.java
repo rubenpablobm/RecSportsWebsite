a335bfbb77beb4657d1ae9062d488ddc6eee4d21
Omar Lopez <a01284179@itesm.mx>
2023-05-02 20:03:35 -0600

    HURF 2
    Como alumno quiero visualizar en área tipo aforo la cantidad de personas que lo están usando en tiempo real para saber si puedo utilizar las instalaciones en ese momento.

--- Pruebas/src/test/java/com/example/pruebas/MainPageTest.java
+++ Pruebas/src/test/java/com/example/pruebas/MainPageTest.java
@@ -1,6 +1,7 @@
 package com.example.pruebas;
 import org.openqa.selenium.support.FindBy;
 import org.testng.annotations.*;
+
 import static org.testng.Assert.*;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
@@ -8,14 +9,16 @@ import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
 import java.time.Duration;
+import java.util.ArrayList;
 import java.util.List;
-
+import org.openqa.selenium.support.ui.WebDriverWait;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import java.util.regex.Pattern;
+import org.openqa.selenium.StaleElementReferenceException;
 public class MainPageTest {
     private WebDriver driver;
     private MainPage mainPage;
 
-
-    public WebElement ulStatic;
 
     @BeforeMethod
     public void setUp() {
@@ -53,12 +56,36 @@ public class MainPageTest {
     //HURF-2 Como alumno quiero visualizar en área tipo aforo la cantidad de personas que lo están usando en tiempo real para saber si puedo utilizar las instalaciones en ese momento.
     @Test
     public void muestraAforo() {
+        WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(10));
+        wait.until(ExpectedConditions.presenceOfAllElementsLocatedBy(By.cssSelector(".area-card")));
         List<WebElement> tarjetasAforo = mainPage.contenedorAforo.findElements(By.cssSelector(".area-card"));
         for(WebElement elemento:tarjetasAforo){
             assertTrue(elemento.findElement(By.cssSelector(".card-title")).isDisplayed());
             assertTrue(elemento.findElement(By.tagName("img")).isDisplayed());
             assertTrue(elemento.findElement(By.xpath(".//label[contains(text(), 'personas')]")).isDisplayed());
             assertTrue(elemento.findElement(By.tagName("progress")).isDisplayed());
+        }
+    }
+
+    @Test
+    public void muestraAreaAforo() {
+        WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(10));
+        wait.until(ExpectedConditions.presenceOfAllElementsLocatedBy(By.cssSelector(".area-card")));
+        List<WebElement> tarjetasAforo = mainPage.contenedorAforo.findElements(By.cssSelector(".area-card"));
+        List<String> areas = new ArrayList<String>();
+        for(WebElement elemento:tarjetasAforo){
+            areas.add(elemento.findElement(By.cssSelector(".card-title")).getText());
+
+        }
+        int index=0;
+        for(WebElement elemento:tarjetasAforo){
+            try {
+                elemento.click();
+                assertTrue(elemento.findElement(By.cssSelector("h1[class='fw-light']")).getText().contains(areas.get(index)));
+            } catch (StaleElementReferenceException e) {
+                continue;
+            }
+
         }
     }
 
