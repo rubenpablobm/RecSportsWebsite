35e075a7a8fb6653fa3731311deab279de47f566
ArturoPatio23 <A00828096@tec.mx>
2023-05-09 11:41:50 -0600

    Comentarios componente acceso

--- RecSports-Libre/src/app/components/acceso/acceso.component.ts
+++ RecSports-Libre/src/app/components/acceso/acceso.component.ts
@@ -1,3 +1,9 @@
+/* Descripcion de acceso.component.ts: programa que define la logica del componente "acceso".
+Su propósito es llamar al servicio API por medio de funciones. 
+  
+Porpiedad del equipo WellSoft.
+Fecha de creacion: 01/05/2023, Fecha de modificacion: 09/05/2023 */
+
 import { Component } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { HttpClient } from '@angular/common/http';
@@ -16,8 +22,11 @@ export class AccesoComponent {
 
   mensaje?:string;
 
+/* Descripcion de ngOnInit: manda llamar al servicio AreaGetXId para obtener el id del area
+ de la ruta y conseguir la informacion del la misma.
+Parametro(s): Ninguno.
+Complejidad ---> O(1): independientemente de del caso, la funcion ejecuta una sola instruccion. */
   ngOnInit() {
-    // obtiene el id del área de la ruta
     const idArea = this.route.snapshot.paramMap.get('idArea');
     this.aID = idArea;
     return this.crudService.AreaGetXId(this.aID).subscribe((data:{}) => {
@@ -26,6 +35,10 @@ export class AccesoComponent {
     })
   }
 
+/* Descripcion de _MasAforo: manda llamar el servicio MasAforo para ejecutar el store procedure
+MasAforo. En caso de error se manda un mensaje. 
+Parametro(s): Ninguno.
+Complejidad ---> O(1): independientemente de del caso, la funcion ejecuta una sola instruccion. */
   _MasAforo(){
     return this.crudService.MasAforo(this.aID).subscribe((data:{})=>{
       console.log(data);
@@ -36,6 +49,10 @@ export class AccesoComponent {
     })
   }
 
+/* Descripcion de _MenosAforo: manda llamar el servicio MenosAforo para ejecutar el store procedure
+MenosAforo. En caso de error se manda un mensaje.
+Parametro(s): Ninguno.
+Complejidad ---> O(1): independientemente de del caso, la funcion ejecuta una sola instruccion. */
   _MenosAforo(){
     return this.crudService.MenosAforo(this.aID).subscribe((data:{})=>{
       console.log(data);
