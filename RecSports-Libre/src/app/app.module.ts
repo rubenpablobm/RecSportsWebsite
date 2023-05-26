e51d5c9f4c827e65dc182ba611d546c7f74b847f
hectorinl <A01411730@itesm.mx>
2023-05-26 09:23:58 -0600

    Cambio contrasena

--- RecSports-Libre/src/app/app.module.ts
+++ RecSports-Libre/src/app/app.module.ts
@@ -22,6 +22,7 @@ import { AgregarEdificioComponent } from './components/agregar-edificio/agregar-
 // import { MatDialogModule } from '@angular/material/dialog'
 import { CompComponent } from './comp/comp.component';
 import { TablaEdificioComponent } from './components/tabla-edificio/tabla-edificio.component';
+import { CambioContrasenaComponent } from './components/cambio-contrasena/cambio-contrasena.component';
 
 @NgModule({
   //Declaracion de componentes
@@ -39,6 +40,7 @@ import { TablaEdificioComponent } from './components/tabla-edificio/tabla-edific
     AgregarEdificioComponent,
     CompComponent,
     TablaEdificioComponent,
+    CambioContrasenaComponent,
 
   ],
   //Modulos importados
