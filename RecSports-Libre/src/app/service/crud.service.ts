3c0e73215132dfbe4644a55ced1e0ddecdf70461
Jesús Sebastián Jaime Oviedo <a01412442@tec.mx>
2023-05-29 10:44:39 -0600

    Adaptaciones para cambiar contra rf-30

--- RecSports-Libre/src/app/service/crud.service.ts
+++ RecSports-Libre/src/app/service/crud.service.ts
@@ -24,7 +24,10 @@ export class CrudService {
   
   mensajeAPI:string="";
 
+  //datos del admin
   logeado:Boolean=false;
+  emailString? : string;
+
   //API: string="angular-test.eastus.cloudapp.azure.com/libros/";
   constructor(private clientehttp:HttpClient) { 
     
@@ -97,14 +100,23 @@ export class CrudService {
 
   /* ADMIN */
   AdminLogin(datosAdmin:Admin):Observable<any>{
-    this.logeado=true;
+    this.logeado=false;
+    this.emailString = datosAdmin.correo;
     return this.clientehttp.post(this.API+"admin/iniciosesion", datosAdmin);
   }
+  AdminLoginAuth(){
+    this.logeado=true;
+  }
   EstaLogeado(){
-    return this.logeado
+    return this.logeado;
+  }
+  EstaLogeadoEmail(){
+    //ya tiene la proteccion de la misma pagina que incluye esta funcion
+    return this.emailString;
   }
   AdminLogout(){
     this.logeado = false;
+    this.emailString = undefined;
     return this.logeado
   }
 
