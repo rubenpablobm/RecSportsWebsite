42cfe7d95f88c906046c676433efba7042b7ce33
Omar Lopez <a01284179@itesm.mx>
2023-05-30 09:23:59 -0600

    Eliminar area crud service

--- RecSports-Libre/src/app/service/crud.service.ts
+++ RecSports-Libre/src/app/service/crud.service.ts
@@ -83,6 +83,10 @@ export class CrudService {
     return this.clientehttp.post(this.API+"area",datosArea);
   }
 
+  AreaDelete(id: number){
+    return this.clientehttp.delete(this.API+"area/"+id)
+  };
+
   /* REGISTRO */
 
   // Incrementar el aforo de un area
