d07b483b0fb1afb4454679b2b34e151b4175620a
Jesús Sebastián Jaime Oviedo <a01412442@tec.mx>
2023-06-12 10:36:53 -0600

    Hotfixes cambio contra

--- Pruebas/src/test/java/com/example/pruebas/ChangePassword.java
+++ Pruebas/src/test/java/com/example/pruebas/ChangePassword.java
@@ -1,11 +1,16 @@
 /* Descripcion MainPageTest.java: programa que define la logica del componente "navbar".
 Su proposito es llamar al servicio API por medio de funciones.
 Porpiedad del equipo WellSoft.
-Ultima edicion por: Arturo Garza Campuzano
+Ultima edicion por: Jesús Sebastián Jaime Oviedo
 Fecha de creacion: 07/06/2023
 Fecha de modificacion: 07/06/2023 */
 
 package com.example.pruebas;
 
 public class ChangePassword {
+    //contraseña incorrecta
+    //checa email
+    //contrasena a cambiar formato incorrecto
+    //contrasena a cambiar formato correcto
+
 }
