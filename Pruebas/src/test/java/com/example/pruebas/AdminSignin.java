5ee397d65b2ebeb8cfc718742640fe887384a4c4
hectorinl <A01411730@itesm.mx>
2023-06-14 21:11:10 -0600

    Pruebas

--- Pruebas/src/test/java/com/example/pruebas/AdminSignin.java
+++ Pruebas/src/test/java/com/example/pruebas/AdminSignin.java
@@ -6,6 +6,39 @@ Fecha de creacion: 07/06/2023
 Fecha de modificacion: 07/06/2023 */
 
 package com.example.pruebas;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
 
 public class AdminSignin {
+
+    @FindBy(css = "h1[class='ini']")
+    public WebElement h1IniciarSesion;
+    @FindBy(css = "input[id='exampleInputEmail1']")
+    public WebElement inputEmail;
+
+    @FindBy(css = "input[id='exampleInputPassword1']")
+    public WebElement inputPassword;
+
+    @FindBy(css = "button[class$='btn-primary']")
+    public WebElement buttonCambioContraseña;
+
+    @FindBy(css = "button[class$='btn-primary']")
+    public WebElement buttonSubmit;
+    @FindBy(css = "html > body > app-root > app-admin > div > div > div:nth-of-type(2) > div:nth-of-type(2)")
+    public WebElement divUsuarioExistente;
+
+    @FindBy(css = "div[class='container-fluid']")
+    public WebElement menuAdmin;
+
+
+    
+    public AdminSignin(WebDriver driver) {
+        PageFactory.initElements(driver, this);
+    }
+    
+    
+    
 }
